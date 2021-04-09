7c856ffc3d5743f1f72511dbab1f6e4dfc609ee3
Veronica Blake/Noyce <vrex149@gmail.com>
2021-04-09 15:32:42 -0600

    init

---
+++ app/Models/Image.js
@@ -0,0 +1,13 @@
+// export default class Image {
+//     constructor({url, large_url}) {
+//         this.url = url
+//         this.largeURL = large_url
+//     }
+
+
+//     get Template(){
+//         return ` 
+//         <img>${this.largeURL}</img>`
+//     }
+
+// }
